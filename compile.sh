e0047eeceb9a1ee9f37b5911cd80fb1afdb7d88f
Nelson Lai <nelsonlai9503@gmail.com>
2022-04-20 02:14:59 +0000

    setup compile

---
+++ compile.sh
@@ -0,0 +1,7 @@
+
+# no need extension
+export CONTRACT_FILE_NAME=StakingPoolCompile
+export COMPANY=net.lateshow.util
+
+solc $CONTRACT_FILE_NAME.sol --bin --abi --optimize --overwrite -o .
+web3j generate solidity -b $CONTRACT_FILE_NAME.bin -a $CONTRACT_FILE_NAME.abi -o . -p $COMPANY
